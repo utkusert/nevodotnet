93ec334be36ea227b5622463beb264f643672986
Utku Sert <93093609+utkusert@users.noreply.github.com>
2024-05-25 16:30:55 +0300

    initial commit

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LogLibrary.Loggers;
+using LogLibrary.Appenders;
+using System.Collections.Generic;
+using System.Linq;
+using LogLibrary.Enums;
+
+namespace LoggingAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogsController : ControllerBase
+    {
+        private readonly LogLibrary.Loggers.ILogger logger;
+        private readonly IEnumerable<IAppender> appenders;
+
+        public LogsController(LogLibrary.Loggers.ILogger logger, IEnumerable<IAppender> appenders)
+        {
+            this.logger = logger;
+            this.appenders = appenders;
+        }
+
+        [HttpPost]
+        public IActionResult LogMessages([FromBody] List<LogMessage> logMessages)
+        {
+            foreach (var logMessage in logMessages)
+            {
+                switch (logMessage?.Level?.ToUpper())
+                {
+                    case "INFO":
+                        logger.Info(logMessage.Time, logMessage.Message);
+                        break;
+                    case "WARNING":
+                        logger.Warn(logMessage.Time, logMessage.Message);
+                        break;
+                    case "ERROR":
+                        logger.Error(logMessage.Time, logMessage.Message);
+                        break;
+                    case "CRITICAL":
+                        logger.Critical(logMessage.Time, logMessage.Message);
+                        break;
+                    case "FATAL":
+                        logger.Fatal(logMessage.Time, logMessage.Message);
+                        break;
+                    default:
+                        return BadRequest("Invalid log level");
+                }
+            }
+
+            var response = new
+            {
+                ConsoleLogs = appenders.OfType<ConsoleAppender>().Select(a => new
+                {
+                    Type = "ConsoleAppender",
+                    LayoutType = a.Layout.GetType().Name,
+                    ReportLevel = a.ReportLevel.ToString(),
+                    MessagesAppended = logMessages.Count(l => Enum.Parse<ReportLevel>(l.Level, true) >= a.ReportLevel)
+                }),
+                FileLogs = appenders.OfType<FileAppender>().Select(a => new
+                {
+                    Type = "FileAppender",
+                    LayoutType = a.Layout.GetType().Name,
+                    ReportLevel = a.ReportLevel.ToString(),
+                    MessagesAppended = logMessages.Count(l => Enum.Parse<ReportLevel>(l.Level, true) >= a.ReportLevel),
+                    LogContent = a.GetLogContent(),
+                    FileSize = a.GetLogContent().Where(char.IsLetter).Sum(c => (int)c)
+                })
+            };
+
+            return Ok(response);
+        }
+    }
+
+    public class LogMessage
+    {
+        public string? Level { get; set; }
+        public string? Time { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+using LogLibrary.Loggers;
+using LogLibrary.Layouts;
+using LogLibrary.Appenders;
+using LogLibrary.Enums;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "LoggingAPI", Version = "v1" });
+});
+
+builder.Services.AddSingleton<LogLibrary.Loggers.ILogger, Logger>(sp =>
+{
+    var simpleLayout = new SimpleLayout();
+    var consoleAppender = new ConsoleAppender(simpleLayout) { ReportLevel = ReportLevel.Critical };
+    var xmlLayout = new XmlLayout();
+    var fileAppender = new FileAppender(xmlLayout) { ReportLevel = ReportLevel.Info };
+    return new Logger(consoleAppender, fileAppender);
+});
+
+builder.Services.AddSingleton<IEnumerable<IAppender>>(sp =>
+{
+    var simpleLayout = new SimpleLayout();
+    var consoleAppender = new ConsoleAppender(simpleLayout) { ReportLevel = ReportLevel.Critical };
+    var xmlLayout = new XmlLayout();
+    var fileAppender = new FileAppender(xmlLayout) { ReportLevel = ReportLevel.Info };
+    return new List<IAppender> { consoleAppender, fileAppender };
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "LoggingAPI v1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
